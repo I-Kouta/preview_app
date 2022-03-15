f9de7e43bc2fc08be6a9cad94f6cbf6d579010c4
I-Kouta <kouta.baseball0222@gmail.com>
2022-03-15 22:24:03 +0900

    選択しなかった画像が削除される記述

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -32,6 +32,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     // 最後のfile_fieldを取得
     const lastFileField = document.querySelector('input[type="file"][name="post[images][]"]:last-child');
+    
     // nextDataIndex = 最後のfile_fieldのdata-index + 1
     const nextDataIndex = Number(lastFileField.getAttribute('data-index')) +1;
     newFileField.setAttribute('data-index', nextDataIndex);
@@ -44,12 +45,27 @@ document.addEventListener('DOMContentLoaded', function(){
     fileFieldsArea.appendChild(newFileField);
   };
 
+  // 指定したdata-indexを持つプレビューとfile_fieldを削除する
+  const deleteImage = (dataIndex) => {
+    const deletePreviewImage = document.querySelector(`.preview[data-index="${dataIndex}"]`);
+    deletePreviewImage.remove();
+    const deleteFileField = document.querySelector(`input[type="file"][data-index="${dataIndex}"]`);
+    deleteFileField.remove();
+  };
+
   // input要素で値の変化が起きた際に呼び出される関数の中身
   const changedFileField = (e) => {
     // data-index（何番目を操作しているか）を取得
     const dataIndex = e.target.getAttribute('data-index');
 
     const file = e.target.files[0];
+
+    // fileが空 = 何も選択しなかったのでプレビュー等を削除して終了する
+    if (!file) {
+      deleteImage(dataIndex);
+      return null;
+    };
+    
     const blob = window.URL.createObjectURL(file);
 
     // data-indexを使用して、既にプレビューが表示されているかを確認する
